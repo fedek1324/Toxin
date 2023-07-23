4c4b1be0ed1fa3e5dd045bc047ef5edc890c5f30
fedek1324 <fedua1324@yandex.ru>
2023-07-23 15:36:02 +0300

    fixed styles and script imports. now not using pug link and script tags

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-import {Calendar} from "@/blocks/form-elements-blocks/input-field/__calendar/calendar";
-import {InputField} from "@/blocks/form-elements-blocks/input-field/b-input-field";
-import {Counter} from "@/blocks/form-elements-blocks/counter/b-counter";
-
-document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('hi');
-
-
-  // doesnt work
-  // function requireAll(requireContext) {
-  //   return requireContext.keys().map(requireContext);
-  // }
-  //
-  // requireAll(require.context('./blocks', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));  // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
-  // requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
-});
+import { Calendar } from './blocks/form-elements-blocks/input-field/__calendar/calendar';
+import { InputField } from './blocks/form-elements-blocks/input-field/b-input-field';
+import { Counter } from './blocks/form-elements-blocks/counter/b-counter';
 
 Calendar.initCalendars();
 Counter.initCounters();
